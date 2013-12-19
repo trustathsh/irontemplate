4fa86379296f9d7d8a2d11176c9d316728937b1f
Bastian Hellmann <bahellma@googlemail.com>
2013-12-19 12:55:10 +0100

    Maven pom.xml, License template, Assembly descriptors, start scripts and Hello World-Class added.

---
+++ src/main/java/de/hshannover/f4/trust/irontemplate/Client.java
@@ -0,0 +1,15 @@
+package de.hshannover.f4.trust.irontemplate;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello World-Class. 
+ */
+public class Client {
+
+	private static Logger LOGGER = Logger.getLogger(Client.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello World!");
+	}
+}
---
+++ src/main/templates/start.bat
@@ -0,0 +1,2 @@
+java -jar irontemplate.jar %*
+

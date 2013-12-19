f3711490b303e3d287ff66a3a99523f6904d9821
Bastian Hellmann <bahellma@googlemail.com>
2013-12-19 13:03:48 +0100

    Fixed wrong file header update

--- src/main/java/de/hshannover/f4/trust/irontemplate/Client.java
+++ src/main/java/de/hshannover/f4/trust/irontemplate/Client.java
@@ -36,3 +36,18 @@
  * limitations under the License.
  * #L%
  */
+package de.hshannover.f4.trust.irontemplate;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello World-Class.
+ */
+public class Client {
+
+	private static Logger LOGGER = Logger.getLogger(Client.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello World!");
+	}
+}

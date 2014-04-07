83b3251afdcc172434ff8e8a1433fe2a812298ee
Bastian Hellmann <bahellma@googlemail.com>
2014-04-07 14:21:26 +0200

    Added license-plugin in pom.xml

--- src/license/dummy/Dummy.java
+++ src/license/dummy/Dummy.java
@@ -36,18 +36,28 @@
  * limitations under the License.
  * #L%
  */
-package de.hshannover.f4.trust.irontemplate;
+=====================================================
+  _____                _     ____  _   _       _   _
+ |_   _|_ __ _   _ ___| |_  / __ \| | | | ___ | | | |
+   | | | '__| | | / __| __|/ / _` | |_| |/ __|| |_| |
+   | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
+   |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
+                            \____/
 
-import org.apache.log4j.Logger;
+=====================================================
 
-/**
- * Hello World-Class.
- */
-public class Client {
+Hochschule Hannover
+(University of Applied Sciences and Arts, Hannover)
+Faculty IV, Dept. of Computer Science
+Ricklinger Stadtweg 118, 30459 Hannover, Germany
 
-	private static Logger LOGGER = Logger.getLogger(Client.class);
+Email: trust@f4-i.fh-hannover.de
+Website: http://trust.f4.hs-hannover.de/
 
-	public static void main(String[] args) {
-		LOGGER.info("Hello World!");
-	}
-}
+This file is part of irontemplate, version 0.0.1,
+implemented by the Trust@HsH research group at the Hochschule Hannover.
+
+<#if addSvnKeyWords>
+<#--Add svn Keywords-->
+${"|Id:|"?replace("|", "$")}
+${"|HeadURL:|"?replace("|", "$")}</#if>
